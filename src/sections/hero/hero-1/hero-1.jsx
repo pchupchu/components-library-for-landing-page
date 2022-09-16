a54476e9ee63649a27a9e93f4b60f421c90091d1
Cyrill Galkin <cyrillgalkin@gmail.com>
2022-09-16 16:52:27 +0300

    Initial commit

---
+++ src/sections/hero/hero-1/hero-1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './hero-1.css'
+
+function Hero1() {
+  return (
+    <div className="hero-1">
+        {/* Here you can write your markup */}
+        <div className='hero-1__title'>Hello World</div>
+    </div>
+  )
+}
+
+export default Hero1
