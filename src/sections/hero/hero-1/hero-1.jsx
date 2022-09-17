dc1595dc96ba6fbed2d063393ec7e53596204310
Marina Malyarchuk <pchupchu@yandex.ru>
2022-09-17 17:31:32 +0300

    change font-size

--- src/sections/hero/hero-1/hero-1.jsx
+++ src/sections/hero/hero-1/hero-1.jsx
@@ -4,25 +4,10 @@ import './hero-1.css';
 function Hero1() {
   return (
     <div className="hero-1">
-<<<<<<< HEAD
-        {/* Here you can write your markup */}
-        <p className='hero-1__logo-name'>Startup 3</p>
-        <h1 className='hero-1__title'>Startup framework</h1>
-=======
       {/* Here you can write your markup */}
       <div className="hero-1__title">Hello World</div>
       <div className="hero-1__subtitle">It's me</div>
->>>>>>> a7155277c15703d52c2c25a7c74dff4816c64d86
     </div>
   );
-}
-
-<<<<<<< HEAD
-export default Hero1
-
-
-
-
-=======
+ 
 export default Hero1;
->>>>>>> a7155277c15703d52c2c25a7c74dff4816c64d86
