ccba6b4f8b3eff373fa625d40f6d642dd01c3543
Cyrill Galkin <cyrillgalkin@gmail.com>
2022-09-17 16:40:39 +0300

    Add DM Sans and Poppins fonts

--- src/sections/hero/hero-1/hero-1.jsx
+++ src/sections/hero/hero-1/hero-1.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import './hero-1.css'
+import React from 'react';
+import './hero-1.css';
 
 function Hero1() {
   return (
     <div className="hero-1">
-        {/* Here you can write your markup */}
-        <div className='hero-1__title'>Hello World</div>
+      {/* Here you can write your markup */}
+      <div className="hero-1__title">Hello World</div>
+      <div className="hero-1__subtitle">It's me</div>
     </div>
-  )
+  );
 }
 
-export default Hero1
+export default Hero1;
